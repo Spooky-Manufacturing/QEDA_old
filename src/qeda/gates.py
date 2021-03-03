3ad07780d586aebdc5e46385e248c40624c789b4
Charles Horn <charles.horn@gmail.com>
2021-03-04 12:51:39 +1300

    remove redundant lines in gates

--- src/qeda/gates.py
+++ src/qeda/gates.py
@@ -1,68 +1,67 @@
 from primitives import U1, U2, U3
 
-global PI
+
 PI = 3.14159
 
+
 class X(U3):
-    global PI
     def __init__(self):
         self.theta = PI
         self.phi = 0
         self.lamda = PI
-        pass
+
 
 class Y(U3):
-    global PI
     def __init__(self):
         self.theta = PI
-        self.phi = PI/2
-        self.lamda = PI/2
-        pass
+        self.phi = PI / 2
+        self.lamda = PI / 2
+
 
 class Z(U1):
-    global PI
     def __init__(self):
         self.lamda = PI
-        pass
+
 
 class H(U2):
-    global PI
     def __init__(self):
         self.phi = 0
         self.lamda = PI
 
+
 class S(U1):
-    global PI
     def __init__(self):
-        self.lamda = PI/2
+        self.lamda = PI / 2
+
 
 class SDG(U1):
-    global PI
     def __init__(self):
-        self.lamda = -PI/2
+        self.lamda = -PI / 2
+
 
 class T(U1):
-    global PI
     def __init__(self):
-        self.lamda = PI/4
+        self.lamda = PI / 4
+
 
 class TDG(U1):
-    global PI
     def __init__(self):
-        self.lamda = -PI/4
+        self.lamda = -PI / 4
+
 
 class RX(U3):
-    global PI
     def __init__(self, theta):
         self.theta = theta
-        self.phi = -PI/2
-        self.lamda = PI/2
+        self.phi = -PI / 2
+        self.lamda = PI / 2
+
 
 class RY(U3):
     def __init__(self, theta):
         self.theta = theta
         self.phi = 0
         self.lamda = 0
+
 
 class RZ(U1):
     def __init__(self, lamda):
